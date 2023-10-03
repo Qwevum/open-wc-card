1da51644968a25859e28a21b7bcfaca106fb5d41
Qwevum <103947732+Qwevum@users.noreply.github.com>
2023-10-03 13:15:29 -0400

    update js

--- src/Wc1.js
+++ src/Wc1.js
@@ -7,6 +7,59 @@ export class Wc1 extends LitElement {
       padding: 25px;
       color: var(--wc-1-text-color, #000);
     }
+    .card {
+      max-width: 400px;
+      margin: 16px;
+      padding: 12px;
+      border: 2px solid #ccc;
+      border-radius: 8px;
+    
+  }
+  
+  .card-img {
+      width: 150px; 
+      margin-right: 12px;
+      border-radius: 8px;
+  }
+  
+  .card-content {
+      flex-grow: 1;
+  }
+  
+  .card-title {
+      font-size: 1.5rem;
+      margin-bottom: 8px;
+      text-align: center
+  }
+  
+  .card-description {
+      margin-bottom: 8px 0;
+      text-align: center
+  }
+  
+  .details-button {
+      display: Details;
+  }
+  
+  @media screen and (min-width: 500px) and (max-width: 800px) {
+      .details-button {
+          display: inline-block;
+      }
+  }
+  
+      .card-img {
+          width: 100%;
+          height: auto;
+          margin-right: 0;
+          border-radius: 0;
+      }
+  }
+  .wrapper{
+    
+  }
+  .highlighted{
+    background-color: red;
+  }
   `;
 
   static properties = {
@@ -22,6 +75,14 @@ export class Wc1 extends LitElement {
 
   __increment() {
     this.counter += 1;
+  }
+
+  duplicate() {
+    const cloneButton = document.getElementById('btn');
+    cloneButton.addEventListener('click', function(){
+      const clonedCard = document.querySelector('.card').cloneNode(true);
+      document.body.appendChild(clonedCard);
+    });
   }
 
   render() {
